eefe712a0669ef2f87c4a6420c62684d709aabd7
EDSON MATEUS ALVES DE OLIVEIRA <tioed2015@gmail.com>
2020-11-25 22:08:46 -0300

    mudanca

--- src/components/Header.js
+++ src/components/Header.js
@@ -1,31 +1,39 @@
-import React, { Component } from 'react';
-import Fade from 'react-reveal/Fade';
-import data from '../yourdata'
-
+import React, { Component } from "react";
+import Fade from "react-reveal/Fade";
+import data from "../yourdata";
 
 class Header extends Component {
-    state = {}
+  state = {};
 
-    render() {
-        return (<div>
-        <h1 className='heading-background'>CREATIVE</h1>
-            <header>
-                <h1>
-                    <Fade bottom cascade>{data.name}</Fade></h1>
-            </header>
-            <Fade bottom>
-            <p className='header-title'>
-            {data.headerTagline[0]}<br></br>{data.headerTagline[1]}<br></br>
-            {data.headerTagline[2]}
-               <br></br>
-                    <button><a href={`mailto:${data.contactEmail}`} rel="noopener noreferrer" >E-mail</a></button>
-                </p>
+  render() {
+    return (
+      <div>
+        <h1 className="heading-background"></h1>
+        <header>
+          <h1>
+            <Fade bottom cascade>
+              {data.name}
             </Fade>
-
-
-
-        </div>);
-    }
+          </h1>
+        </header>
+        <Fade bottom>
+          <p className="header-title">
+            {data.headerTagline[0]}
+            <br></br>
+            {data.headerTagline[1]}
+            <br></br>
+            {data.headerTagline[2]}
+            <br></br>
+            <button>
+              <a href={`mailto:${data.contactEmail}`} rel="noopener noreferrer">
+                E-mail
+              </a>
+            </button>
+          </p>
+        </Fade>
+      </div>
+    );
+  }
 }
 
 export default Header;
